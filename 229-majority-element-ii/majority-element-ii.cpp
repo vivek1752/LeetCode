7bd2b7997c24d2157d07b6611dbe0f34888a1c22
vivek1752 <167667069+vivek1752@users.noreply.github.com>
2024-04-22 22:43:54 +0530

    Time: 6 ms (85.84%) | Memory: 18.5 MB (35.90%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -5,38 +5,25 @@ using namespace std;
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+    vector<int> ls;
+    map <int,int> mpp;
+    int n = nums.size();
+    int mini = (int)(n/3)+1;
 
-        vector<int >arr;
-        int count=0;
-        
-        for (int i=0; i<nums.size(); i++)
+    for (int i = 0; i<n; i++)
+    {
+        mpp[nums[i]]++;
+
+        if (mpp[nums[i]]==mini)
         {
-                count=0;
-           for (int j=i; j<nums.size(); j++)
-            {
-                if (nums[j]==nums[i])
-                {
-                    count++;
-                };
-
-            }
-
-            if(arr.size()==2)
-            {
-                break;
-            };
-
-            if((nums.size()/3)<count)
-            {
-                if (find(arr.begin(), arr.end(), nums[i]) == arr.end()) {
-                    arr.push_back(nums[i]);
-                };
-            };
-
-
-
+            ls.push_back(nums[i]);
         }
-    return arr;
+        if (ls.size()==2) break;
+    }
+
+    sort(ls.begin(),ls.end());
+
+    return ls;
 
     }
 
