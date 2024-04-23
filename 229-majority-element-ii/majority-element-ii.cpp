66f2fbcfcafd6ad62a0d02b090f8c81fbc17de40
vivek1752 <167667069+vivek1752@users.noreply.github.com>
2024-04-23 11:59:02 +0530

    Time: 19 ms (7.91%) | Memory: 18.5 MB (47.06%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -5,26 +5,56 @@ using namespace std;
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-    vector<int> ls;
-    map <int,int> mpp;
-    int n = nums.size();
-    int mini = (int)(n/3)+1;
 
-    for (int i = 0; i<n; i++)
-    {
-        mpp[nums[i]]++;
+        int count1=0; int count2=0;
+        int ele1=INT_MIN; int ele2=INT_MIN;
 
-        if (mpp[nums[i]]==mini)
+        for (int i=0; i<nums.size(); i++)
+        // {
+        //     if (count1==0 && ele1!=nums[i])
+        //     {
+        //         count1=1;
+        //         ele1=nums[i];
+        //     }else if (count2==0 && ele2!=nums[i]){
+        //         count2=1;
+        //         ele2=nums[i];
+        //     }else if (ele1==nums[i])count1++;
+        //     else if (ele2==nums[i])count2++;
+        //     else {
+        //         count1--; count2--;
+        //     }
+        // }
+       {
+        
+            if (ele1==nums[i])
+                count1++;
+            else if (ele2==nums[i])
+                count2++;
+            else if (count1==0) {
+                count1=1;
+                ele1=nums[i];
+            } else if (count2==0) {
+                count2=1;
+                ele2=nums[i];
+            } else {
+                count1--; count2--;
+            }
+       } 
+        vector <int> out;
+        count1=0; count2=0;
+
+        for (int i=0; i<nums.size(); i++)
         {
-            ls.push_back(nums[i]);
+            if (ele1 == nums[i]) count1++;
+            if (ele2 == nums[i]) count2++;
+            
+           
         }
-        if (ls.size()==2) break;
-    }
-
-    sort(ls.begin(),ls.end());
-
-    return ls;
-
+        int mini = ((int)(nums.size()/3));
+         if (count1>mini)out.push_back(ele1);
+         if (count2>mini)out.push_back(ele2);
+         sort(out.begin(), out.end());
+       return out; 
     }
 
 
