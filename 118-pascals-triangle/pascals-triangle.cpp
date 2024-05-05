206a0df4774bfb36b52df342f412c25e6271abd2
vivek1752 <167667069+vivek1752@users.noreply.github.com>
2024-05-05 16:26:03 +0530

    Time: 4 ms (34.98%) | Memory: 7.9 MB (78.78%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> generateRow(int row) {
+        long long ans = 1;
+        vector<int> ansRow;
+        ansRow.push_back(1);
+
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / (col);
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pascalTri;
+        for (int i = 1; i <= numRows; i++) {
+            vector<int> temp = generateRow(i);
+            pascalTri.push_back(temp);
+        }
+
+        return pascalTri;
+    }
+};
