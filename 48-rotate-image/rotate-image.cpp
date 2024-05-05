428a74bbbc56f2f342c2c5b5cac8297c029cafd3
vivek1752 <167667069+vivek1752@users.noreply.github.com>
2024-05-05 12:28:05 +0530

    Time: 4 ms (38.75%) | Memory: 8.7 MB (21.49%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,17 +4,15 @@ public:
 
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> nm(n, vector<int>(n));
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                nm[j][n - 1 - i] = matrix[i][j];
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < m; j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                matrix[i][j] = nm[i][j];
-            }
+
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
